7ab4ce88fab677f859227441e8aa73c830ac1fd0
piyush <piyushkkumarr@outlook.com>
2024-02-22 11:19:02 +0530

    Data added. Sort done.

--- index.js
+++ index.js
@@ -3,9 +3,13 @@ document.addEventListener("DOMContentLoaded", async function () {
     const response = await fetch("data.json");
     const data = await response.json();
 
+    //Using IN-BUILT FUNCTION
+    // data.sort((a, b) => a.employeeId.localeCompare(b.employeeId));
+
+    data.sort((a, b) => (a.employeeId > b.employeeId ? 1 : -1));
+
     const tableBody = document.getElementById("tableBody");
 
-    // Looping through the employees(data.json) and adding rows to the table
     data.forEach((employee) => {
       const row = tableBody.insertRow();
       row.insertCell(0).textContent = employee.employeeId;
