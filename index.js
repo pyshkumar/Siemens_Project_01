48954635919e84b95ef87326bb8d1c89eee93bf0
piyush <piyushkkumarr@outlook.com>
2024-02-26 15:23:42 +0530

    Sort configuration for ID added

--- index.js
+++ index.js
@@ -1,23 +1,37 @@
 document.addEventListener("DOMContentLoaded", async function () {
   try {
     const response = await fetch("data.json");
-    const data = await response.json();
+    let data = await response.json();
 
-    //Using IN-BUILT FUNCTION
-    // data.sort((a, b) => a.employeeId.localeCompare(b.employeeId));
-
-    data.sort((a, b) => (a.employeeId > b.employeeId ? 1 : -1));
-
+    const toggleButton = document.querySelector(".switch input");
     const tableBody = document.getElementById("tableBody");
 
-    data.forEach((employee) => {
-      const row = tableBody.insertRow();
-      row.insertCell(0).textContent = employee.employeeId;
-      row.insertCell(1).textContent = employee.firstName;
-      row.insertCell(2).textContent = employee.lastName;
-      row.insertCell(3).textContent = employee.email;
-      row.insertCell(4).textContent = employee.contactNumber;
-      row.insertCell(5).textContent = employee.position;
+    const populateTable = (sort) => {
+      data.sort((a, b) =>
+        sort
+          ? a.employeeId.localeCompare(b.employeeId)
+          : b.employeeId.localeCompare(a.employeeId)
+      );
+
+      tableBody.innerHTML = "";
+
+      data.forEach((employee) => {
+        const row = tableBody.insertRow();
+        row.insertCell(0).textContent = employee.employeeId;
+        row.insertCell(1).textContent = employee.firstName;
+        row.insertCell(2).textContent = employee.lastName;
+        row.insertCell(3).textContent = employee.email;
+        row.insertCell(4).textContent = employee.contactNumber;
+        row.insertCell(5).textContent = employee.position;
+      });
+    };
+
+    populateTable(true);
+
+    toggleButton.addEventListener("change", function () {
+      const sort = this.checked;
+
+      populateTable(sort);
     });
   } catch (error) {
     console.error(error);
